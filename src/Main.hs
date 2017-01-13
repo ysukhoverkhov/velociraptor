4a233896fe8d841f0393223af4918b1c1403c538
Yury <ysukhoverkhov@locomote.com>
2017-01-13 21:31:31 +0300

    Test auth request to github.

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,34 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
+import qualified Data.ByteString.Lazy.Char8 as LS8
+import qualified Data.ByteString.Char8      as S8
+-- import           Data.Aeson                 (Value)
+-- import qualified Data.Yaml                  as Yaml
+import           Network.HTTP.Simple
 
-import Control.Monad
+main :: IO ()
+main = do
 
-main = doStuff
+    -- https://haskell-lang.org/library/http-client
+    let token = "github token here"
 
-doStuff = do
-    putStrLn "Input a line"
-    line <- getLine
-    putStrLn . rev $ line
-    unless(null line) doStuff
-    where rev = unwords . reverse . words
+    let request
+            = setRequestHost "api.github.com"
+            $ setRequestMethod "GET"
+            $ setRequestPath "/"
+            $ setRequestHeaders [("Authorization", S8.pack("token " ++ token)), ("User-Agent", "Awesome-Octocat-App")]
+            $ setRequestSecure True
+            $ setRequestPort 443
+            $ defaultRequest
+    response <- httpLBS request
+
+    putStrLn $ "The status code was: " ++
+               show (getResponseStatusCode response)
+    putStrLn $ "Content-Type" ++
+               show (getResponseHeader "Content-Type" response)
+    LS8.putStrLn $ getResponseBody response
+
+
+
+
+
+    -- https://wiki.haskell.org/High-level_option_handling_with_GetOpt
