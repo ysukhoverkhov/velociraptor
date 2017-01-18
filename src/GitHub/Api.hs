724b22e90a0cec949b9d3ee845af40c3b63cc5bb
Yury <ysukhoverkhov@locomote.com>
2017-01-19 01:49:31 +0300

    Commits are fetched (almost).

--- src/GitHub/Api.hs
+++ src/GitHub/Api.hs
@@ -2,37 +2,50 @@
 {-# LANGUAGE DeriveGeneric #-}
 
 module GitHub.Api (
-    Repo, ErrorDescription,
-    Auth (..), RepoSource (..), Error (..),
-    repos) where
+    Repo (..), ErrorDescription (..),
+    Auth (..), RepoSource (..), Error (..), CommitsCriteria (..),
+    repos, commits) where
 
 import           GHC.Generics (Generic)
 import           Control.Arrow (left)
 import           Control.Monad ((>=>))
+import           Data.Monoid ((<>))
 import qualified Data.ByteString.Lazy.Char8 as LS8
 import qualified Data.ByteString.Char8      as S8
 import qualified Network.HTTP.Simple        as HTTP
 import qualified Data.Aeson                 as Aeson
+import qualified Data.Time.Clock            as Clock
 
 -- TODO: use Text
 
 -- GitHub domain types
 
-data Repo = Repo {
-    id :: Int,
-    owner :: Owner,
-    name :: String,
-    full_name :: String
-} deriving (Generic, Show)
-
-data Owner = Owner {
-    login :: String
-} deriving (Generic, Show)
-
 data ErrorDescription = ErrorDescription {
     message :: String
 } deriving (Generic, Show)
 
+data Repo = Repo {
+    id :: Int,
+    owner :: Person,
+    name :: String,
+    full_name :: String
+} deriving (Generic, Show)
+
+data Person = Person {
+    login :: String
+} deriving (Generic, Show)
+
+data Commit = Commit {
+    sha :: String,
+    author :: Person
+} deriving (Generic, Show)
+
+-- TODO: switch to GHC 8.0
+-- data CommitPerson = CommitPerson {
+--     name :: String,
+--     email :: String,
+--     date :: String
+-- } deriving (Generic, Show)
 
 -- API types
 
@@ -40,38 +53,51 @@ data Auth = Auth {
     token :: String
 } deriving (Show)
 
-data RepoSource = Own | User String | Organization String deriving (Show)
-
 data Error =
     InvalidPayload {payload :: String, parserError :: String} |
     GitHubApiError {statusCode :: Int, errorDescription :: ErrorDescription}
     deriving (Show)
+
+data RepoSource = Own | User String | Organization String deriving (Show)
+
+data CommitsCriteria = CommitsCriteria {
+    repoFullName :: String,
+    since :: Maybe Clock.UTCTime,
+    until :: Maybe Clock.UTCTime
+} deriving (Show)
 
 
 -- Api
 
 repos :: Auth -> RepoSource -> IO (Either Error [Repo])
 repos auth source =
-     HTTP.httpLBS request >>= return . parseResponse
---     HTTP.httpLBS request >>= print >> return (Right [])
-    where request = reposRequest source $ authenticatedRequest auth githubRequest
+    performRequest $ reposRequest source $ authenticatedRequest auth githubRequest
+
+commits :: Auth -> CommitsCriteria -> IO (Either Error [Commit])
+commits auth criteria =
+    performRequest $ commitsRequest criteria $ authenticatedRequest auth githubRequest
+
+performRequest :: (Aeson.FromJSON a) => HTTP.Request -> IO (Either Error a)
+performRequest request = HTTP.httpLBS request >>= return . parseResponse
 
 
 -- Payloads fetching
 
 bodyFromResponse :: HTTP.Response LS8.ByteString -> Either Error LS8.ByteString
-bodyFromResponse response = pure $ HTTP.getResponseBody response
+bodyFromResponse response = pure $ HTTP.getResponseBody response -- TODO: think about jumping into functor here.
 
--- TODO: Should we move these strings to sort of constants?
-reposRequest Own =
-    let endpoint = "/user/repos"
-    in  HTTP.setRequestPath endpoint
-reposRequest (User name) =
-    let endpoint a = "/users/" ++ a ++ "/repos"
-    in  HTTP.setRequestPath . S8.pack . endpoint $ name
-reposRequest (Organization name) =
-    let endpoint a = "/orgs/" ++ a ++ "/repos"
-    in  HTTP.setRequestPath . S8.pack . endpoint $ name
+reposRequest source =
+    HTTP.setRequestPath endpoint
+    where endpoint = case source of
+            Own               -> "/user/repos"
+            User name         -> "/users/" <> S8.pack name <> "/repos"
+            Organization name -> "/orgs/" <> S8.pack name <> "/repos"
+
+
+-- TODO: take time frame into account.
+commitsRequest criteria =
+    (HTTP.setRequestPath . S8.pack $ "/repos/" ++ repoFullName criteria ++ "/commits") .
+    HTTP.setRequestQueryString [("since", Just $ S8.pack "2011-04-14T16:00:49Z")]
 
 authenticatedRequest auth =
     HTTP.addRequestHeader "Authorization" (S8.pack ("token " ++ token auth))
@@ -102,15 +128,19 @@ parsePayload json =
     where wrapAesonError aesonError = InvalidPayload {payload = LS8.unpack json, parserError = aesonError}
 
 
+instance Aeson.FromJSON ErrorDescription
+instance Aeson.ToJSON ErrorDescription where
+    toEncoding = Aeson.genericToEncoding Aeson.defaultOptions
+
 instance Aeson.FromJSON Repo
 instance Aeson.ToJSON Repo where
     toEncoding = Aeson.genericToEncoding Aeson.defaultOptions
 
-instance Aeson.FromJSON Owner
-instance Aeson.ToJSON Owner where
+instance Aeson.FromJSON Person
+instance Aeson.ToJSON Person where
     toEncoding = Aeson.genericToEncoding Aeson.defaultOptions
 
-instance Aeson.FromJSON ErrorDescription
-instance Aeson.ToJSON ErrorDescription where
+instance Aeson.FromJSON Commit
+instance Aeson.ToJSON Commit where
     toEncoding = Aeson.genericToEncoding Aeson.defaultOptions
 
--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,33 @@
 import GitHub.Api
 
+import Data.Time
+import Data.Time.Format
+
+
 main :: IO ()
-main =
-    let token = "insert token here."
-    in  repos Auth { token = token } Own >>= print
+main = do
+    let token = "token here"
+    let auth = Auth { token = token }
+
+    myRepos <- repos auth Own
+    print myRepos
+
+    case myRepos of
+        Right xs -> do
+            let s = parseTimeM False defaultTimeLocale "%-d-%-m-%Y" "1-1-2012"
+            let u = parseTimeM False defaultTimeLocale "%-d-%-m-%Y" "1-1-2015"
+
+            case s of
+                Just t -> print $ formatTime defaultTimeLocale "%Y/%m/%d %H:%M" t
+                _ -> print "not parsed"
+
+            c <- commits auth CommitsCriteria { repoFullName = full_name . head $ xs, since = s, GitHub.Api.until = u }
+            print c
+        Left error -> print error
+
+
+printRepoCommits :: Repo -> IO ()
+printRepoCommits repo = do
+    return ()
 
     -- https://wiki.haskell.org/High-level_option_handling_with_GetOpt
